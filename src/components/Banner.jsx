04822e8af6d4fe5c35045558f88e73254f53f186
Kevin Taulman <48804521+ktaulman@users.noreply.github.com>
2022-08-17 13:40:48 -0400

    updated portfolio link in <Banner> and added resume for buttons

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -20,15 +20,16 @@ export default function Banner() {
           <div className="placeholder-gray-300 mt-3 flex justify-evenly items-center">
             {/* //Resume Tag */}
             <a
-              href="/"
+              href="/resume.pdf"
               className=" text-sky-600 text-lg font-bold px-2 py-1 rounded border-2 border-sky-600 hover:bg-sky-600 hover:text-white"
+              target="_blank"
             >
               View Resume
             </a>
-            <a>
+            <a href='https://github.com/ktaulman' target='_blank'>
               <img className="w-10" src={githubLogo} />
             </a>
-            <a>
+            <a href='https://www.linkedin.com/in/ktaulman/' target='_blank'>
               <img className="w-10" src={linkedinLogo} />
             </a>
           </div>
